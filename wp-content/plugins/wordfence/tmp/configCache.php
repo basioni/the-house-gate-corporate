9e5f86485926192a199348fb2a838e419f811841
basioni <eng.ahmed.bassioni@gmail.com>
2023-04-04 02:50:35 +0200

    Release v1.0

---
+++ wp-content/plugins/wordfence/tmp/configCache.php
@@ -0,0 +1,4 @@
+<?php
+/* Wordfence temporary file security header */
+echo "Nothing to see here!\n"; exit(0);
+?>a:22:{s:9:"cacheType";s:0:"";s:9:"howGetIPs";s:0:"";s:11:"whitelisted";s:0:"";s:6:"apiKey";s:160:"7594b66af99d83b8424b52823b3e5abfdd46d97d220d7ff9d0801979505d4dc64e10145cf576d66d51ccba98ecbc5b46e6a868d6fb9c5edbd0acdfd1af72b2ee171011737a1aa726b963020a611f3a3c";s:20:"loginSec_lockoutMins";s:3:"240";s:19:"other_hideWPVersion";s:1:"1";s:15:"firewallEnabled";s:1:"1";s:6:"isPaid";s:0:"";s:11:"blockedTime";s:3:"300";s:12:"neverBlockBG";s:18:"neverBlockVerified";s:13:"blockFakeBots";s:1:"0";s:10:"bannedURLs";s:0:"";s:18:"other_blockBadPOST";s:1:"0";s:18:"liveTrafficEnabled";s:1:"1";s:14:"disableCookies";s:1:"0";s:10:"tourClosed";s:0:"";s:17:"actUpdateInterval";s:0:"";s:7:"debugOn";s:1:"0";s:13:"welcomeClosed";s:1:"1";s:38:"email_summary_dashboard_widget_enabled";s:1:"1";s:22:"email_summary_interval";s:8:"biweekly";s:11:"other_WFNet";s:1:"1";}
